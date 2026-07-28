7d5e4f4b509811d23b45651e54011c89a54d7cfe
nullptr <mtedeschi@uri.edu>
2026-07-28 15:25:54 -0400

    feat: build.sh now produces app bundle

--- build.sh
+++ build.sh
@@ -1,1 +1,18 @@
+#!/bin/sh
+set -eu
+
+APP_NAME="Vim BTW"
+BUNDLE_DIR="$APP_NAME.app"
+CONTENTS_DIR="$BUNDLE_DIR/Contents"
+MACOS_DIR="$CONTENTS_DIR/MacOS"
+RESOURCES_DIR="$CONTENTS_DIR/Resources"
+EXECUTABLE_NAME="vim-btw"
+
 swift build -c release
+
+mkdir -p "$MACOS_DIR" "$RESOURCES_DIR"
+cp ".build/release/$EXECUTABLE_NAME" "$MACOS_DIR/$EXECUTABLE_NAME"
+cp "Packaging/Info.plist" "$CONTENTS_DIR/Info.plist"
+chmod +x "$MACOS_DIR/$EXECUTABLE_NAME"
+
+printf 'Built %s\n' "$BUNDLE_DIR"
